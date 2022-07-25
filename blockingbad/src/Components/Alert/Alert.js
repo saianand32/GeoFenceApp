2d203514f8b4ae9e967b4ad5714094d8204fffd3
SAISHWAR ANAND <saishwaranand32@gmail.com>
2022-07-25 20:42:51 +0530

    test

--- blockingbad/src/Components/Alert/Alert.js
+++ blockingbad/src/Components/Alert/Alert.js
@@ -8,16 +8,31 @@ const url = 'http://127.0.0.1:8000/fencepoints/boy'
 
 function Alert() {
 
-  const [bool, setBool]  = useState("false")
+  const [bool, setBool]  = useState("false");
+  const [str,setStr] = useState("Dormant....")
+  const [borders,setBorders] = useState("wheat");
 
   const activateGeo=async()=>{
     try {
       let resp = await axios.get(url);
       setBool(resp.data.toString());
-      console.log("hii")
+      let a = resp.data.toString();
+    
+
+      setStr("Active !!!")
+
+      if(a=="true"){
+        setBorders("green");
+      }
+      if(a=="false")
+      {
+        setBorders("red");
+      }
+
       setTimeout(() => {
         activateGeo();
       }, 3000);
+
     } catch (error) {
       console.log(error.response.data);
     }
@@ -31,8 +46,8 @@ function Alert() {
     <div className='alertContainer' style={{marginTop:"40vh"}}>
 
     {/* <h1 >boolval = {mode}</h1> */}
-    <h1 >Your GeoFence is {bool} !!!</h1>
-    <div className="alertanim">
+    <h1 >Your GeoFence is {bool}</h1>
+    <div className={borders}>
 
     </div>
     <button className='activateButton' onClick={activateGeo} style={{background:"white"}}>Activate Geofence</button>
